87d72e14afca7d7eee4440a302c26799da26ee6a
stijn buurman <stijnbuurman@gmail.com>
2018-04-30 21:38:37 +0200

    Added tests for RetryState.

---
+++ spec/RetryState/RetryState.spec.ts
@@ -0,0 +1,29 @@
+import { assert } from 'chai';
+import 'mocha';
+import { RetryState } from '../../src';
+
+describe('Retry State', () => {
+  it('should set retryCount to 0 if no parameter is passed in constructor', () => {
+    const retryState = new RetryState();
+    assert.equal(retryState.getRetryCount(), 0);
+  });
+
+  describe('addOneRetry', () => {
+    it('should return a new object retryState with one more retry', () => {
+      const retryState = new RetryState();
+      assert.equal(retryState.getRetryCount(), 0);
+
+      const newRetryState = retryState.addOneRetry();
+      assert.equal(newRetryState.getRetryCount(), 1);
+    });
+
+    it('should be immutable', () => {
+      const retryState = new RetryState();
+      assert.equal(retryState.getRetryCount(), 0);
+
+      const newRetryState = retryState.addOneRetry();
+      assert.equal(retryState.getRetryCount(), 0);
+      assert.equal(newRetryState.getRetryCount(), 1);
+    });
+  });
+});
