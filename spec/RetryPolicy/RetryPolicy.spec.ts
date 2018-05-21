25fdff81c20c9314fdd2179413557c86f13f4f46
stijn buurman <stijnbuurman@gmail.com>
2018-05-21 20:45:02 +0200

    feat: Add RetryPolicy.execute to make it easier to use
    RetryPolicy now returns an object with two methods: execute and handleError

--- spec/RetryPolicy/RetryPolicy.spec.ts
+++ spec/RetryPolicy/RetryPolicy.spec.ts
@@ -1,5 +1,8 @@
 import { RetryPolicy, RetryState } from '../../src';
-import { noErrorDetectionStrategy } from '../../src/ErrorDetectionStrategy';
+import {
+  genericErrorDetectionStrategy,
+  noErrorDetectionStrategy
+} from '../../src/ErrorDetectionStrategy';
 
 import * as chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
@@ -9,19 +12,21 @@ import { afterAttemptStopStrategy } from '../../src/StopStrategy';
 chai.use(chaiAsPromised);
 const assert = chai.assert;
 
-describe('RetryPolicy', () => {
+describe('RetryPolicy - HandleError', () => {
   describe('Retryable errors', () => {
     const retryPolicy = RetryPolicy({});
 
     it('should resolve on a retryable error', () => {
-      return assert.isFulfilled(retryPolicy(new Error()));
+      return assert.isFulfilled(retryPolicy.handleError(new Error()));
     });
 
     it('should return the correct retryState', () => {
-      return retryPolicy(new Error()).then((retryState: RetryState) => {
-        assert.equal(retryState.getRetryCount(), 1);
-        assert.equal(retryState.getLastError().name, 'Error');
-      });
+      return retryPolicy
+        .handleError(new Error())
+        .then((retryState: RetryState) => {
+          assert.equal(retryState.getRetryCount(), 1);
+          assert.equal(retryState.getLastError().name, 'Error');
+        });
     });
   });
 
@@ -31,22 +36,31 @@ describe('RetryPolicy', () => {
     });
 
     it('should resolve on first 3 retryable errors and fail after', async () => {
-      const retryStatePromise = retryPolicy(new Error());
+      const retryStatePromise = retryPolicy.handleError(new Error());
       assert.isFulfilled(retryStatePromise);
 
       retryStatePromise
         .then((retryState: RetryState) => {
-          const retryStatePromise2 = retryPolicy(new Error(), retryState);
+          const retryStatePromise2 = retryPolicy.handleError(
+            new Error(),
+            retryState
+          );
           assert.isFulfilled(retryStatePromise2);
           return retryStatePromise2;
         })
         .then((retryState: RetryState) => {
-          const retryStatePromise3 = retryPolicy(new Error(), retryState);
+          const retryStatePromise3 = retryPolicy.handleError(
+            new Error(),
+            retryState
+          );
           assert.isFulfilled(retryStatePromise3);
           return retryStatePromise3;
         })
         .then((retryState: RetryState) => {
-          const retryStatePromise4 = retryPolicy(new Error(), retryState);
+          const retryStatePromise4 = retryPolicy.handleError(
+            new Error(),
+            retryState
+          );
           assert.isRejected(retryStatePromise4);
         });
     });
@@ -59,7 +73,32 @@ describe('RetryPolicy', () => {
         stopStrategy: afterAttemptStopStrategy({ attempts: 5 })
       });
 
-      assert.isRejected(retryPolicy(new RangeError()));
+      assert.isRejected(retryPolicy.handleError(new RangeError()));
+    });
+  });
+});
+
+describe('RetryPolicy - Execute', () => {
+  describe('Retryable errors', () => {
+    const retryPolicy = RetryPolicy({
+      errorDetectionStrategies: [
+        genericErrorDetectionStrategy({
+          errors: [RangeError]
+        })
+      ],
+      stopStrategy: afterAttemptStopStrategy({ attempts: 5 })
+    });
+
+    it('should resolve on not an error', () => {
+      return assert.isFulfilled(
+        retryPolicy.execute(() => Promise.resolve(true))
+      );
+    });
+
+    it('should reject on a non retryable error', () => {
+      return assert.isRejected(
+        retryPolicy.execute(() => Promise.reject(new Error()))
+      );
     });
   });
 });
--- src/RetryPolicy/RetryPolicy.ts
+++ src/RetryPolicy/RetryPolicy.ts
@@ -1,7 +1,10 @@
 import { RetryState } from '../';
-import { allErrorDetectionStrategy, IsRetryable } from '../ErrorDetectionStrategy';
+import {
+  allErrorDetectionStrategy,
+  IsRetryable
+} from '../ErrorDetectionStrategy';
 import { delayWithPass } from '../helpers/helpers';
-import { IsStopped, neverStopStrategy} from '../StopStrategy';
+import { IsStopped, neverStopStrategy } from '../StopStrategy';
 import { GetWaitTime, linearWaitStrategy } from '../WaitStrategy';
 
 export const RetryPolicy = ({
@@ -26,13 +29,31 @@ export const RetryPolicy = ({
     return Promise.reject(error);
   }
 
-  return (
+  function handleError(
     error: Error,
     retryState: RetryState = new RetryState()
-  ): Promise<RetryState | Error> => {
+  ): Promise<RetryState | Error> {
     return errorDetectionStrategies.some(isRetryable => isRetryable(error)) &&
       !stopStrategy(retryState.getRetryCount())
       ? handleRetryable(error, retryState)
       : handleFatal(error);
+  }
+
+  function executeAsync<T>(
+    action: (...args) => Promise<any>,
+    retryState: RetryState = new RetryState()
+  ): Promise<T> {
+    return action().catch(error => {
+      return handleError(error, retryState).then(
+        (newRetryState: RetryState) => {
+          return executeAsync(action, newRetryState);
+        }
+      );
+    });
+  }
+
+  return {
+    execute: executeAsync,
+    handleError
   };
 };
